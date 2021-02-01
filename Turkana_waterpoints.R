16c929a9b3a59e6378eb3d4adaf168165b4aeaee
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-02-01 23:11:41 +0300

    Getting to the 10th waterpoint in the county.

--- Turkana_waterpoints.R
+++ Turkana_waterpoints.R
@@ -4,14 +4,17 @@ library(tidyverse)
 dams <- data.frame(longitude = c(35.582391, 35.581789, 
                                  35.579651, 35.579189, 
                                  35.577966, 35.577676,
-                                 35.577371, 35.355020),
+                                 35.577371, 35.355020,
+                                 35.247397, 35.254445),
                    latitude = c(3.222909, 3.222657, 
                                 3.224472, 3.224581,
                                 3.225090, 3.225225,
-                                3.225368, 3.148446),
-                   listing = c(1:8),
+                                3.225368, 3.148446,
+                                3.144302, 3.144950),
+                   listing = c(1:10),
                    Sub_County = c('Loima', 'Loima', 'Loima', 'Loima',
-                                  'Loima', 'Loima', 'Loima', 'Loima'))
+                                  'Loima', 'Loima', 'Loima', 'Loima',
+                                  'Loima', 'Loima'))
 
 
 
