8431414f1f95f779fd66ae710b1801b5abf223d9
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-02-02 16:56:21 +0300

    Getting to the 21st water point.

--- Turkana_waterpoints.R
+++ Turkana_waterpoints.R
@@ -1,21 +1,23 @@
 library(leaflet)
-library(tidyverse)
+library(dplyr)
 
 dams <- data.frame(longitude = c(35.582391, 35.581789, 35.579651, 35.579189, 
                                  35.577966, 35.577676, 35.577371, 35.355020,
                                  35.247397, 35.254445, 35.425808, 35.454905,
                                  35.398920, 35.512867, 35.639222, 35.697148,
-                                 35.783488, 35.767541, 35.632926, 35.793876),
+                                 35.783488, 35.767541, 35.632926, 35.793876,
+                                 35.795057),
                    latitude = c(3.222909, 3.222657, 3.224472, 3.224581,
                                 3.225090, 3.225225, 3.225368, 3.148446,
                                 3.144302, 3.144950, 3.110151, 4.626087,
                                 4.650788, 4.659210, 3.192118, 4.633935,
-                                4.209630, 4.268374, 4.313147, 4.115460),
-                   listing = c(1:20),
+                                4.209630, 4.268374, 4.313147, 4.115460,
+                                4.114035),
+                   listing = c(1:21),
                    Sub_County = c('Loima', 'Loima', 'Loima', 'Loima',
                                   'Loima', 'Loima', 'Loima', 'Loima',
                                   'Loima', 'Loima', 'Loima', 'NA', 'NA', 
-                                  'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'))
+                                  'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA', 'NA'))
 
 dams <- dams %>% mutate(popup_info = paste('Sub_County: ', Sub_County, "<br/>", 
                                            'Number: ',listing, "<br/>",
@@ -26,6 +28,6 @@ leaflet() %>% addTiles() %>%
   addCircleMarkers(data = dams, 
                    lat = ~latitude,
                    lng = ~longitude, 
-                   radius = ~3,
+                   radius = ~12,
                    popup = ~popup_info)
 
