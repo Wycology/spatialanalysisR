40e0c5f968307ea49c1ba9bb1e591833acee5784
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-03-11 15:19:20 +0300

    calculations on predictions

--- sd_model_outpputs.R
+++ sd_model_outpputs.R
@@ -5,6 +5,8 @@
 # Relevance: This is super helpful with appreciating uncertainty around model
 # outputs...like where do the models disagree most and where to they agree more
 
+# This script is not standalone it continues from prediction stage of sdm
+
 # In running predict function, we generated several layers and stored as .img 
 # files as specified in the finename = "blabla.img".
 
@@ -14,14 +16,16 @@
 predicted_layers <- predict(model, predictors, 
                             filename = 'predicted_bands.tif') # Note the .tif
 
-# This is saved as a single layer with several bands (methods*replications*reps)
+# This is saved as a single layer with several bands. Each band represent model id
+# (methods*replications*reps). So number 1 can be SVM under bootsrapping for 
+# first rep.
 
 # To read this into R, use stack function in R
 
 stacked_layers <- stack('predicted_bands.tif') #Reads all the bands. If you use raster,
 # only one band will be read at a time.
 
-stacked_layers # This shows that nlayers are 30 (equal to the modl outputs)
+stacked_layers # This shows that nlayers are 30 (equal to my model outputs)
 
 # Now we can calculate median, mean, sd et al using calc function in raster
 
