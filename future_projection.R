3747e1d764c8ccad38c9e989898c2812967966c8
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-03-12 15:21:48 +0300

    as need be

--- future_projection.R
+++ future_projection.R
@@ -7,7 +7,16 @@ getData('CMIP5', var='tmin', res=10, rcp=85, model='AC', year=70)
 
 # A more complete one will look like:
 
-
+getData(name = 'CMIP5', # others would be worldclim, GADM, alt, SRTM(plus lon,lat)
+        var = 'tmin',   # others include tmax, prec, and bio
+        model = 'AC',   # others "BC", "CC", "CE", "CN", "GF", "GD", "GS", "HD", "HG", "HE", "IN", "IP", "MI", "MR", "MC", "MP", "MG", or "NO"
+        rcp = 45,       # others 26, 60 and 85
+        year = 50,      # other 70
+        res = 10,       # others 2.5 and 5
+        lon = 35,       # as need be
+        lat = -0.23,    # as need be
+        download = T, 
+        path = 'D:/JUNK')
 
 function (var, model, rcp, year, res, lon, lat, path, download = TRUE)
   
