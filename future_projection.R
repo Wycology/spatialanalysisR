1ebdb302c2bcb15312aee5cb4d1d1c8c43ebf352
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-03-12 15:03:25 +0300

    Making future projection

--- future_projection.R
+++ future_projection.R
@@ -2,3 +2,15 @@
 
 # This is super simple, the beginning is to download the future climate scenario
 
+getData('CMIP5', var='tmin', res=10, rcp=85, model='AC', year=70)
+
+# Here is a sample code for downloading the future scenario 
+
+
+function (var, model, rcp, year, res, lon, lat, path, download = TRUE)
+  
+  'model' should be one of "AC", "BC", "CC", "CE", "CN", "GF", "GD", "GS", "HD", "HG", "HE", "IN", "IP", "MI", "MR", "MC", "MP", "MG", or "NO".
+
+'rcp' should be one of 26, 45, 60, or 85.
+
+'year' should be 50 or 70
