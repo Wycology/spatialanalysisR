9b605f340929dd51a99d262b34c341670b69f961
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-03-12 15:07:25 +0300

    Explaining the parameters when downloading the future climate scenarios.

--- future_projection.R
+++ future_projection.R
@@ -1,16 +1,15 @@
 # Making a projection into the future
 
 # This is super simple, the beginning is to download the future climate scenario
+# Here is a sample code for downloading the future scenario 
 
 getData('CMIP5', var='tmin', res=10, rcp=85, model='AC', year=70)
-
-# Here is a sample code for downloading the future scenario 
 
 
 function (var, model, rcp, year, res, lon, lat, path, download = TRUE)
   
-  'model' should be one of "AC", "BC", "CC", "CE", "CN", "GF", "GD", "GS", "HD", "HG", "HE", "IN", "IP", "MI", "MR", "MC", "MP", "MG", or "NO".
+# 'model' should be one of "AC", "BC", "CC", "CE", "CN", "GF", "GD", "GS", 
+# "HD", "HG", "HE", "IN", "IP", "MI", "MR", "MC", "MP", "MG", or "NO".
+# 'rcp' should be one of 26, 45, 60, 80----
+# 'year' should be 50 or 70
 
-'rcp' should be one of 26, 45, 60, or 85.
-
-'year' should be 50 or 70
