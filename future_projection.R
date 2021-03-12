bd30ebbc5021277c3b76411003b4bf05db83315e
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-03-12 15:16:44 +0300

    Complete

--- future_projection.R
+++ future_projection.R
@@ -5,7 +5,7 @@
 
 getData('CMIP5', var='tmin', res=10, rcp=85, model='AC', year=70)
 
-
+# A more complete one
 function (var, model, rcp, year, res, lon, lat, path, download = TRUE)
   
 # 'model' should be one of "AC", "BC", "CC", "CE", "CN", "GF", "GD", "GS", 
