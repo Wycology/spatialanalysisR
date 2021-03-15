6ffdfaef981ae10b1c98f797ac175e0ef1fb51b0
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-03-15 19:54:37 +0300

    Reproject one to the other

--- mask_irregular.R
+++ mask_irregular.R
@@ -22,7 +22,7 @@ worldclim <- getData('worldclim', var = 'tmin', res = 10)
 kenya <- getData('GADM', country = 'KEN', level = 0)
 
 # Check crs of your raster and polygon, they should be the same. If not, check
-# on how to reproject either to the other.
+# on how to re-project either to the other.
 
 crs(worldclim) # CRS arguments: +proj=longlat +datum=WGS84 +no_defs 
 crs(kenya)     # CRS arguments: +proj=longlat +datum=WGS84 +no_defs 
