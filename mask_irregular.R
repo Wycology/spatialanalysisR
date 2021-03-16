f1bab2cfb3887113300017e2279e0604c2d51b87
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-03-16 11:00:24 +0300

    data

--- mask_irregular.R
+++ mask_irregular.R
@@ -2,7 +2,7 @@
 # As a response to an email on 8th March 2021
 # Last edited on 16th March 2021
 
-# Clipping occurrence records and predictor variables with your study area
+# Clipping occurrence records data and predictor variables with your study area
 # boundary
 
 # Libraries for the project at hand
