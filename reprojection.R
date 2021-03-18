e3c2a67dafb4f5b3103e9d288ade6850cef2fbbe
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2021-03-18 14:05:20 +0300

    geosphere

--- reprojection.R
+++ reprojection.R
@@ -29,7 +29,8 @@ library(mapview)    # Rendering html maps..version 2.9.0
 library(leaflet)    # Rendering html maps too..version 2.0.4.1    
 library(grid)       # Did something..version 4.0.4
 library(cartogram)  # Another map making package..version 0.2.2
-library()
+library(geosphere)  # For calculating distance between coordinates
+
 # st_is_longlat is one great function for checking if geographic CRS is in use
 
 # Here we start by a dummy dataframe and check for crs
