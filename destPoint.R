2c09ee1cae3eef173043d7e8b49d04d892988290
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-04-12 11:27:58 +0300

    last one

--- destPoint.R
+++ destPoint.R
@@ -5,3 +5,9 @@ library(sp)
 library(geosphere)
 
 destPoint(p = c(35, 3), b = 60, d = 125)
+
+the_mat <- data.frame(lon = c(35, 35.2, 35.6),
+                      lat = c(3, 3.2, 3.6))
+the_matrix <- as.matrix(the_mat)
+
+destPoint(p = the_matrix, b = abs(rnorm(3, 100, 30)), d = rnorm(3, 150, 25))
