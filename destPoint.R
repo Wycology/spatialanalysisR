c4f81819ff886eeeb37fb050da581f9662273e09
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-04-12 11:07:51 +0300

    the two libraries

--- destPoint.R
+++ destPoint.R
@@ -1,4 +1,5 @@
 # Calculating the coordinates of the destination point given the starting point 
 # in degrees, bearing in degrees, and distance in metres
 
-library()
+library(sp)
+library(geosphere)
