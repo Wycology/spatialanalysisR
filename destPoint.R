b2a22037b88a9f0b3189a0776f5af85f86a06e04
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-04-18 11:43:05 +0300

    task

--- destPoint.R
+++ destPoint.R
@@ -1,6 +1,6 @@
 
 
-# Calculating the coordinates of the destination point given the starting point 
+# TaskCalculating the coordinates of the destination point given the starting point 
 # in degrees, bearing in degrees, and distance in metres
 
 library(sp)
