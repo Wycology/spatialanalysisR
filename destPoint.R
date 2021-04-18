e72bb60d6c266bd33d8d3ab22ca6e4a970c96b93
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-04-18 11:45:01 +0300

    Oluoch

--- destPoint.R
+++ destPoint.R
@@ -1,4 +1,4 @@
-# Created by Wyclife Agumba
+# Created by Wyclife Agumba Oluoch    
 
 # Task: Calculating the coordinates of the destination point given the starting 
 # point in degrees, bearing in degrees, and distance in metres
