796376f5a5b768a279d018f607643a7d7598c7c6
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-05-05 08:31:55 +0300

    Awesome capability of mapsf package version 0.2.0

--- mapsf.R
+++ mapsf.R
@@ -63,9 +63,46 @@ mf_inset_off()
 
 # world map inset
 
+mf_map(mtq)
 
+mf_inset_on(x = 'worldmap')
 
+mf_worldmap(mtq)
 
+mf_inset_off()
+
+# Non-geographic insets, such as histograms
+
+bks <- mf_get_breaks(x = mtq$MED, nbreaks = 5, breaks = 'quantile')
+bks <- round(bks, digits = -2)
+
+pal <- hcl.colors(n = 5, palette = 'Dark Mint', rev = TRUE)
+
+mf_theme('candy')
+
+fg <- mf_theme()$fg
+
+mf_map(x = mtq, var = 'MED', type = 'choro', 
+       pal = pal, breaks = bks, leg_pos = NA)
+
+mf_inset_on(fig = c(0.75, .95, .84, .99))
+
+par(mar = c(0, 0, 1.7, 0))
+
+hist(mtq$MED, breaks = bks, col = pal, border = fg, axes = F, labels = '',
+     xlab = '', ylab = '', main = '')
+
+axis(side = 1, at = bks, las = 2, tick = FALSE, line = -.9,
+     cex.axis = .7, col.axis = fg)
+
+title('Median Income\nin euros', cex.main = .8, col.main = fg,
+      font.main = 1, adj = 0)
+
+mf_inset_off()
+
+mf_title('Wealth in Martinique, 2015', pos = 'left')
+mf_scale(5)
+mf_credits(paste0('Sources: Insee and IGN, 2018\n', 'mapsf ', packageVersion('mapsf')))
 
 
 
