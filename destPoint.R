e38515866c4689fa2567cd2eb8c11b13ffeaa63c
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-07-15 22:56:01 +0300

    the coords

--- destPoint.R
+++ destPoint.R
@@ -9,7 +9,7 @@
 
 library(geosphere) # The packagefor the analyses
 
-destPoint(p = c(35, 3), b = 60, d = 125)
+destPoint(p = c(35, 3), b = 60, d = 125) # Gives 
 
 the_mat <- data.frame(lon = c(35, 35.2, 35.6),
                       lat = c(3, 3.2, 3.6))
