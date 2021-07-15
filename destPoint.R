aff21dfd92d0050d52585a48bd35c4e5d9eb1eaa
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-07-15 22:57:01 +0300

    bearing and distance

--- destPoint.R
+++ destPoint.R
@@ -10,7 +10,7 @@
 library(geosphere) # The packagefor the analyses
 
 destPoint(p = c(35, 3), b = 60, d = 125) # Gives the coords of a point which is 
-                                        # 
+                                        # bearing and distance
 
 the_mat <- data.frame(lon = c(35, 35.2, 35.6),
                       lat = c(3, 3.2, 3.6))
