3da59cbf9db1805ad1e64d5232ff784cb8906bf3
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-07-15 22:55:00 +0300

    the package

--- destPoint.R
+++ destPoint.R
@@ -7,8 +7,7 @@
 # Task: Calculating the coordinates of the destination point given the starting 
 # point in degrees, bearing in degrees, and distance in metres
 
-library(sp)
-library(geosphere)
+library(geosphere) 
 
 destPoint(p = c(35, 3), b = 60, d = 125)
 
@@ -17,3 +16,4 @@ the_mat <- data.frame(lon = c(35, 35.2, 35.6),
 the_matrix <- as.matrix(the_mat)
 
 destPoint(p = the_matrix, b = abs(rnorm(3, 100, 30)), d = rnorm(3, 150, 25))
+
