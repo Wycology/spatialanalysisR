31cdd46b8e7bf62184e5169c1820cd34eb155682
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-07-15 22:55:14 +0300

    the package

--- destPoint.R
+++ destPoint.R
@@ -7,7 +7,7 @@
 # Task: Calculating the coordinates of the destination point given the starting 
 # point in degrees, bearing in degrees, and distance in metres
 
-library(geosphere) 
+library(geosphere) # The package 
 
 destPoint(p = c(35, 3), b = 60, d = 125)
 
