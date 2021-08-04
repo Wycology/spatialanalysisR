5516ef2a6d855d0fdf2a13ef8d39c0c9d914d516
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-08-04 15:16:45 +0300

    base

--- reprojection.R
+++ reprojection.R
@@ -21,7 +21,7 @@ base::library(spDataLarge)# Has larger data than spData above..version 0.5.1
 base::library(mapview)    # Rendering html maps..version 2.9.0
 base::library(leaflet)    # Rendering html maps too..version 2.0.4.1    
 base::library(grid)       # Did something..version 4.0.4
-library(cartogram)  # Another map making package..version 0.2.2
+base::library(cartogram)  # Another map making package..version 0.2.2
 library(geosphere)  # For calculating distance between coordinates
 
 # st_is_longlat is one great function for checking if geographic CRS is in use
