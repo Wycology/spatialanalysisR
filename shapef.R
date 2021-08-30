a4d8f45f9201679d5c587695b263775a3f0651fd
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-08-30 17:22:35 +0300

    30th

--- shapef.R
+++ shapef.R
@@ -1,4 +1,7 @@
 # Working with shapefile in R as df
+# Created on
+# Last edited on 30th August 2021 by the author
+
 base::library(rgdal) # library for working with shapefile in R
 
 mySHP <- base::file.choose() # Choosing the shapefile from directory    
