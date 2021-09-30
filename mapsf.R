b9a375e3497da835a7b890bf37b25163c93d75a9
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-09-30 14:52:35 +0300

    c

--- mapsf.R
+++ mapsf.R
@@ -16,7 +16,7 @@ mapsf::mf_theme(x = 'candy',
          mar = c(0, 0, 0, 0),
          )
 
-mtq <- mapsf::mf_get_mtq() # Loading Martinique sf data to the R environment
+mtq <- mapsf::mf_get_mtq() # Load data
 
 mtq$geom # Looking at the geometry column of the data. Projected coords   
 
