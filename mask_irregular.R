cf12f4aab692a3e4e933a4164830303c8e622e33
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-11-24 20:05:24 +0300

    c

--- mask_irregular.R
+++ mask_irregular.R
@@ -64,7 +64,7 @@ occurrence_cropped <- crop(occurrence, kenya) # Only occurrence within kenya ext
 plot(clim_masked[[1]])
 plot(occurrence_cropped, add = T) # Yes cropped, but with kenyan extent.
 
-# Do the 'masking' of occurrence data
+# Do the 'masking' of occurrence
 
 # Start by converting occurrence_cropped to sf object, since my kenya is an sf
 # object
