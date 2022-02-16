48a22f1215bf3f14419aacf063c82bb67bd6f9fb
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-02-16 21:06:09 +0300

    c

--- mask_irregular.R
+++ mask_irregular.R
@@ -18,7 +18,7 @@ library(sf)      # version 1.0.4
 
 worldclim <- getData('worldclim', var = 'tmin', res = 10)
 
-# study area boundary I get from GADM database
+# study area boundary I ge from GADM database
 
 kenya <- getData('GADM', country = 'KEN', level = 0)
 
