e06160869138f9148b246e589ce6bc6de39e3b96
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-02-21 12:56:59 +0300

    c

--- future_projection.R
+++ future_projection.R
@@ -2,8 +2,3 @@
 # Created on 13th May 2021                      #
 # Last edited on 1st November 2021              #
 # Author: Wyclife Agumba Oluoch                 #
-# Last edited by the author on 21st October 2021#
-# Projection into the future                    #
-#################################################
-
-library(raster)
