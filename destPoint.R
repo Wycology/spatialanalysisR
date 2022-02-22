3c853b0609a9189a8ef8e5bbdc278def1b68f78d
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-02-22 08:20:30 +0300

    c

--- destPoint.R
+++ destPoint.R
@@ -1,7 +1,7 @@
 ####################################
 # Created by Wyclife Agumba Oluoch    
 # Created on: 19th Apr 2021
-# Last edited on 30th Fe 2022
+# Last edited on 30th Feb 2022
 ####################################
 
 # Task: Calculating the coordinates of the destination point given the starting 
