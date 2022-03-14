c99a3d0b20f9e10cf85fbd7c1e4dc2920082719d
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2022-03-14 20:54:35 +0100

    c

--- mapsf.R
+++ mapsf.R
@@ -98,7 +98,7 @@ mf_worldmap(mtq)
 
 mf_inset_off()
 
-# Non-geographic insets, s  histograms
+# Non-geographic insets,  histograms
 
 bks <- mf_get_breaks(x = mtq$MED, nbreaks = 5, breaks = 'quantile')
 bks <- round(bks, digits = -2)
