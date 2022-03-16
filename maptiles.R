e8cf5db94f3d99bb1cc310186b661203abaa3f23
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-03-16 20:31:46 +0100

    c

--- maptiles.R
+++ maptiles.R
@@ -1,6 +1,6 @@
 # remotes::install_github('riatelab/maptiles')
 base::library(maptiles) # For loading the tiles
-base::library(sf) # for simple features handling
+base::library(sf) # for simple features handli
 
 nc_raw <- sf::st_read(base::system.file("shape/nc.shp", package = 'sf'), quiet = TRUE)
 
