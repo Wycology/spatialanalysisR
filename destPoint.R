11575b01f96a6480b8eaa4cd741c0a7ed2397918
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-04-12 20:45:08 +0200

    c

--- destPoint.R
+++ destPoint.R
@@ -4,7 +4,7 @@
 # Last edited on 11th April 2022    #
 #####################################
 
-# Task: Calculating the coordinates of the destination point given the starting 
+# Task: Calculating th coordinates of the destination point given the starting 
 # point in degrees, bearing in degrees, and distance in metres
 
 library(geosphere) # Loading the library
