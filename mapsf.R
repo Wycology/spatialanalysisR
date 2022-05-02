e2437b65f55e8a5298bc8b20cd2a0453cd4d0deb
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-05-02 21:20:40 +0200

    c

--- mapsf.R
+++ mapsf.R
@@ -1,7 +1,7 @@
 # Task: Creating maps and inset maps
 # Author: Wyclife Agumba Oluoch
 # Contact: https://github.com/Wycology
-# Last edited 16th A 2022
+# Last edited 16th  2022
 
 library(mapsf)
 
