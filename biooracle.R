feba3e8869db2aef820ccabf5afc33c94062650b
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-05-04 19:41:11 +0200

    c

--- biooracle.R
+++ biooracle.R
@@ -37,7 +37,7 @@ environment.bottom <- load_layers( layercodes = c("BO2_tempmax_bdmean" ,
                                    equalarea=FALSE, rasterstack=TRUE)
 
 
-# Download bathymetry dat
+# Download bathymetry data
 bathymetry <- load_layers("BO_bathymean")
 
 # Generate a data.frame with the sites of interest
