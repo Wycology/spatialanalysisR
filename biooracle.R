ff4878325ffcaa2bb533cb56acfab32f23122e73
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-05-04 19:41:45 +0200

    c

--- biooracle.R
+++ biooracle.R
@@ -40,7 +40,7 @@ environment.bottom <- load_layers( layercodes = c("BO2_tempmax_bdmean" ,
 # Download bathymetry data
 bathymetry <- load_layers("BO_bathymean")
 
-# Generate adata.frame with the sites of interest
+# Generate data.frame with the sites of interest
 my.sites <- data.frame(Name = c("Faro, Portugal, NE Atlantic" , 
                               "Maspalomas, Spain, NE Atlantic" , 
                               "Guadeloupe, France, Caribbean Sea" , 
