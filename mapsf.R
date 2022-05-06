64f2f1c2f7228543c502f2b17ea52f03f6bcfd0e
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-05-06 18:41:57 +0200

    Loading default data the come with the package

--- mapsf.R
+++ mapsf.R
@@ -16,7 +16,7 @@ mapsf::mf_theme(x = 'candy',
          mar = c(0, 0, 0, 0),
          )
 
-mtq <- mapsf::mf_get_mtq() # Load data
+mtq <- mapsf::mf_get_mtq() # Load the default data coming with the package
 
 mtq$geom # Looking at the geometry column of the data. Projected coords   
 
