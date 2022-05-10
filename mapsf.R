f1b12209345a3ffe52f6bbb66512bce42d75d658
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-05-10 23:34:34 +0200

    Terminating the inset map

--- mapsf.R
+++ mapsf.R
@@ -207,7 +207,7 @@ mf_map(mtq, add = TRUE) # Add another layer to the map.
 
 mf_title(txt = 'darkula') # Assigning title to the plotted map
 
-dev.off() # Stopping the inset
+dev.off() #  the inset
 
 mf_map(mtq)
 
