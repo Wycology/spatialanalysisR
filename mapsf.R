b926f41b0c6a4b237ae2a2b601969256d60e5b6c
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-05-26 22:08:53 +0200

    c

--- mapsf.R
+++ mapsf.R
@@ -50,6 +50,7 @@ mf_inset_off() # stopping the inset function from running
 mf_theme('dark') # picking the dark theme of mapsf to do the mapping
 
 mtq_target <- mtq[26,] # picking the right map to plot out
+
 mf_map(mtq) # mapping the sf object to the plots window
 
 mf_map(mtq_target, add = TRUE, col = 'tomato') # tomato is the color, fun
