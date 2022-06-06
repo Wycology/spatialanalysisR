ee0714c3e5e99736e448d0dbab79171e414376db
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-06-06 18:29:28 +0200

    Replacing NAs in several columns with respective column  median

--- repace_nas.R
+++ repace_nas.R
@@ -1,3 +1,4 @@
+library(dplyr)
 
 column1 <- c(4, 9, 10, NA, 5, 12, NA, 7, 11, 8)
 column2 <- c(4, NA, 10, 7, 5, 12, 12, 7, NA, 8)
@@ -9,7 +10,7 @@ my_df <- data.frame(column1, column2, column3, column4, column5)
 
 my_df
 
-library(dplyr)
+
 library(tidyr)
 
 my_df |>mutate(across(where(is.numeric), ~ replace_na(., median(., na.rm = TRUE))))
