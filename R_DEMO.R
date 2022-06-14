0f15aa8a8f303a412de19df3de399f251c934f65
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-06-14 21:46:59 +0200

    Update the R demo file

--- R_DEMO.R
+++ R_DEMO.R
@@ -7,7 +7,7 @@ r <- raster(nrow = 10, ncol = 10) # Creating raster of 100 cells
 r[] <- rnorm(1:ncell(r)) # Filling the raster with random values
 
 names(r) <- "min_temp" # Re-naming the raster layer
-df <- data.frame(r = as.data.frame(r), coordinates(r))|> dplyr::arrange(min_temp) # Dataframe the raster 
+df <- data.frame(r = as.data.frame(r), coordinates(r))|>dplyr::arrange(min_temp) # Dataframe the raster 
  
 df <- df |> mutate(pa = case_when(min_temp <= -1 ~ 1,
                                   min_temp > -1 ~ 0,
