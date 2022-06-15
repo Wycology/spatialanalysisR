68a0eec85a8895b0e82e3b77573dbc5a3dac7948
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-06-15 22:36:47 +0200

    Working with shapefile in R as data frame

--- shapef.R
+++ shapef.R
@@ -1,6 +1,6 @@
 # Working with shapefile in R as data a frame
 # Created on 30th August 2021
-# Last edited on 1th May 2022 by the author
+# Last edited on 15th May 2022 by the author
 
 library(rgdal)        # Package version 1.5.31
 library(tidyverse)    # Package version 1.3.1
