3b677b11d26c0ae79ac0e9b7742485452eae92a4
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-06-29 23:37:57 +0200

    Updating the script to 29th June 2022

--- biooracle.R
+++ biooracle.R
@@ -2,7 +2,7 @@
 # Created by Wyclife Agumba Oluoch                                    #
 # Date 22nd Apr 2021                                                  #
 # Working with bathymetry data to predict marine species distribution #
-# Last edited 24th June 2022                                          #
+# Last edited 29th June 2022                                          #
 #######################################################################
 
 library(sdmpredictors)  # version 0.2.12
