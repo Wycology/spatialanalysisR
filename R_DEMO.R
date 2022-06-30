793b3f275d157fe63fdf1be50aa5468f46214c8d
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-06-30 23:34:03 +0200

    Adding versions of the packages used in the R demo

--- R_DEMO.R
+++ R_DEMO.R
@@ -3,7 +3,7 @@ library(dplyr)  # version 1.0.9
 library(sdm)    # version 1.1.8
 
 set.seed(2014) # Setting seed for reproducibility reasons.
-r <- raster(nrow = 10, ncol = 10) # Creating raster of 100 (10 rows and 10 columns) cells
+r <- raster(nrow = 10, ncol = 10) # Creating raster of 100 (10 rows and 10 columns) cells.
 r[] <- rnorm(1:ncell(r)) # Filling the raster with random values of length equal to
 
 names(r) <- "min_temp" # Re-naming the raster layer
