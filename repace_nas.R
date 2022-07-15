8c050fbd1cf68c20535ec2b68ed953a812a3a2f9
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-07-15 14:53:36 +0200

    Replacing NAs across columns with median

--- repace_nas.R
+++ repace_nas.R
@@ -2,13 +2,15 @@
 
 library(dplyr) # Version 1.0.9
 library(tidyr) # Version 1.2.0
+library(tibble) # Version 3.1.7
 
-column1 <- c(4, 9, 10, NA, 5, 12, NA, 7, 11, 8)
-column2 <- c(rep(NA, 10))
-column3 <- c(rep(NA, 8), 11, 8)
-column4 <- c(NA, 9, 10, 12, 5, 12, NA, 7, 11, 8)
-column5 <- c(NA, NA, NA, NA, NA, NA, NA, NA, 0, -9999)
+my_tibble <- tribble(
+  ~column1, ~column2, ~column3, ~column4, ~column5,
+  4, 9, 10, NA, 5, 12, NA, 7, 11, 8,
+  NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
+  NA, NA, NA, NA, NA, NA, NA, NA, 11, 8,
+  NA, 9, 10, 12, 5, 12, NA, 7, 11, 8,
+  NA, NA, NA, NA, NA, NA, NA, NA, 0, -9999
+)
 
-my_df <- data.frame(column1, column2, column3, column4, column5)
-
-my_df |> mutate(across(where(is.numeric), ~ replace_na(., median(., na.rm = TRUE))))
+my_tibble |> mutate(across(where(is.numeric), ~ replace_na(., median(., na.rm = TRUE))))
