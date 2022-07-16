da60d60358784d57045bc9f9e87602b080356f06
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-07-16 17:47:09 +0200

    Working with NAs in tibble

--- repace_nas.R
+++ repace_nas.R
@@ -4,13 +4,12 @@ library(dplyr) # Version 1.0.9
 library(tidyr) # Version 1.2.0
 library(tibble) # Version 3.1.7
 
-my_tibble <- tribble(
-  ~column1, ~column2, ~column3, ~column4, ~column5,
-  4, 9, 10, NA, 5, 12, NA, 7, 11, 8,
-  NA, NA, NA, NA, NA, NA, NA, NA, NA, NA,
-  NA, NA, NA, NA, NA, NA, NA, NA, 11, 8,
-  NA, 9, 10, 12, 5, 12, NA, 7, 11, 8,
-  NA, NA, NA, NA, NA, NA, NA, NA, 0, -9999
-)
+my_tibble <- tibble(column1 = c(4, 9, 10, NA, 5, 12, NA, 7, 11, 8), 
+                    column2 = c(2, 1, 4, 5, 6, 7, 4, 3, 8, 12), 
+                    column3 = c(NA, NA, NA, NA, NA, NA, NA, NA, NA, NA),
+                    column4 = c(NA, NA, NA, NA, NA, NA, NA, NA, 11, 8), 
+                    column5 = c(NA, NA, NA, NA, NA, NA, NA, NA, 11, 8), 
+                    column6 = c(NA, 9, 10, 12, 5, 12, NA, 7, 11, 8),
+                    column7 = c(NA, NA, NA, NA, NA, NA, NA, NA, 0, -9999))
 
 my_tibble |> mutate(across(where(is.numeric), ~ replace_na(., median(., na.rm = TRUE))))
