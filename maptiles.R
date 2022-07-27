a528d2e36a018ea88a4687eadf209ec9878a2e69
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-07-27 17:01:31 +0200

    Updating the water points in Turkana County

--- maptiles.R
+++ maptiles.R
@@ -1,6 +1,6 @@
 # remotes::install_github('riatelab/maptiles')
 library(maptiles) # For loading the tiles
-library(sf) # 
+library(sf) # For 
 
 nc_raw <- st_read(system.file("shape/nc.shp", package = 'sf'), 
                       quiet = TRUE)
