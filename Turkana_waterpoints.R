283b375eeca86d60c927fa3c686bca5aee866f36
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-07-27 17:00:28 +0200

    Updating the water points in Turkana County

--- Turkana_waterpoints.R
+++ Turkana_waterpoints.R
@@ -1,7 +1,7 @@
 ####################################################
 # Created by Wyclife Agumba Oluoch                 #     
 # Created on 22nd April 2021                       #
-# Last edited on 27n July 2022                     #
+# Last edited on 27 July 2022                     #
 # Task: Mapping open water points in Turkana County#
 ####################################################
 
