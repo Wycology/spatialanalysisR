816f87dea0ce1e299c0134f84f39d0e5db7b9d83
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-08-02 16:53:25 +0200

    Setting all the codes to date in August 2022

--- mapsf.R
+++ mapsf.R
@@ -1,7 +1,7 @@
 # Task: Creating maps and inset maps
 # Author: Wyclife Agumba Oluoch
 # Contact: https://github.com/Wycology
-# Last edited 29th J 2022
+# Last edited 29th  2022
 
 library(mapsf) # Loads the main mapping package.
 
