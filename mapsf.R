d70064589472c4c12bd68544d38c1fa81c1d072c
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-08-24 17:06:43 +0200

    spatR

--- mapsf.R
+++ mapsf.R
@@ -43,7 +43,7 @@ mf_scale(size = 2,
 
 mf_map(mtq_target, add = FALSE, col = 'tomato') # mapping the inset
 
-mf_inset_off() # Stop the inset function from runnin
+mf_inset_off() # Stop the inset function from runni
 
 # Set theme for project
 
