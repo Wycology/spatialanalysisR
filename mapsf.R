5c0afb95a69459bab84271a5b3d8110690a587a0
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-08-30 20:49:11 +0200

    Committing the script

--- mapsf.R
+++ mapsf.R
@@ -49,7 +49,7 @@ mf_inset_off() # Stop the inset function
 
 mf_theme('dark') # Picking dark theme do the mapping
 
-mtq_target <- mtq[26,] # Pick the right map to plot 
+mtq_target <- mtq[26,] # Pick the right map to plot
 
 mf_map(mtq) # Plotting the sf object to the plots wind
 
