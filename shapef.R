72d4585b420817485be4e31d4e664964051233c1
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-09-04 22:45:17 +0200

    Shapef

--- shapef.R
+++ shapef.R
@@ -5,7 +5,7 @@
 library(rgdal)        # Package version 1.5.32
 library(tidyverse)    # Package version 1.3.2
 library(broom)        # Package version 1.0.1
-library(patchwork)    # Package version 1.1.1
+library(patchwork)    # Package version 1.1.
 
 my_shapefile <- readOGR('shp/dummy.shp')
 
