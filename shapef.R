fc3f1a80074cc45c85e7b632edbe117f06e83268
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-09-06 22:11:03 +0200

    shapefR

--- shapef.R
+++ shapef.R
@@ -32,7 +32,7 @@ my_tibble |> ggplot(aes(long, lat, group = group, col = group)) +
   coord_map('mercator') +
   geom_path()
 
-# Plotting without  coordinates
+# Plotting with coordinates
 a <- my_tibble |> ggplot(aes(long, lat, group = group, col = group)) +
   geom_path()
 
