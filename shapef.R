80ec4534d0194481311325f528cd4566b465812b
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-09-16 21:34:58 +0200

    shapef.R

--- shapef.R
+++ shapef.R
@@ -1,6 +1,6 @@
 # Working with shapefile in R as data a frame      #
 # Created on 30th August 2021                      #
-# Last edited on 8th September 2022 by the author  #
+# Last edited on th September 2022 by the author  #
 
 library(rgdal)        # Version 1.5.32
 library(tidyverse)    # Version 1.3.2
@@ -14,15 +14,9 @@ length(my_shapefile)   # Number of features in the layer...like two roads
 
 my_tibble <- tidy(my_shapefile) # Tidy the shapefile
 
-class(my_tibble) 
-
 head(my_tibble) 
 
 nrow(my_tibble)
-
-su
-
-glimpse(my_tibble)
 
 # Making map
 
@@ -34,12 +28,14 @@ my_tibble |> ggplot(aes(long, lat, group = group, col = group)) +
 
 # Plotting regardless of coordinates
 a <- my_tibble |> ggplot(aes(long, lat, group = group, col = group)) +
-  geom_path()
+  geom_path() +
+  labs(x = "Longitude", y = "Latitude")
 
 # Consideration the spatial nature of the data
 b <- my_tibble |> ggplot(aes(long, lat, group = group, col = group)) +
   coord_map('mercator') +
-  geom_path()
+  geom_path() +
+  labs(x = "Logitude", y = "Latitude")
 
 b / a
 
