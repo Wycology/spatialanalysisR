272e70b4319f89d62e05bb625008344039416b66
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-09-28 18:10:41 +0200

    Spatial distance

--- destPoint.R
+++ destPoint.R
@@ -4,7 +4,7 @@
 # Last edited on 4th September 2022 #
 #####################################
 
-# Task: Calculate the coordinates of destination given the starting 
+# Task: Calculate thecoordinates of destination given the starting 
 # point coordinates in degrees, bearing in degrees, while distance in meters
 
 library(geosphere) # Loading the geosphere package
