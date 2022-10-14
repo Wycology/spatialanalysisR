9646edff215871875fd8e83853a91276cba4c565
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-10-14 22:54:30 +0300

    Replacing missing values with NAs

--- repace_nas.R
+++ repace_nas.R
@@ -2,7 +2,7 @@
 
 library(dplyr) # Version 1.0.10
 library(tidyr) # Version 1.2.1
-library(tibble) # Version 3.1.7
+library(tibble) # Version 3.1.8
 
 my_tibble <- tibble(column1 = c(4, 9, 10, NA, 5, 12, NA, 7, 11, 8), 
                     column2 = c(2, 1, 4, 5, 6, 7, 4, 3, 8, 12), 
