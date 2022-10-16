4c5ce88201d8b00aa7798ec33acf59c30f98dd60
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-10-16 21:09:48 +0300

    Working with bioracle data

--- biooracle.R
+++ biooracle.R
@@ -2,7 +2,7 @@
 # Created by Wyclife Agumba Oluoch                                    #
 # Date 22nd Apr 2021                                                  #
 # Working with bathymetry data to predict marine species distribution #
-# Last edited 16th O  2022                                          #
+# Last edited 16th Oc  2022                                          #
 #######################################################################
 
 library(sdmpredictors)  # version 0.2.12
