c0fb12d887a1a42273dd49e816f03967306cc6c1
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-10-17 23:43:55 +0300

    working with biooracle

--- biooracle.R
+++ biooracle.R
@@ -52,7 +52,7 @@ my.sites <- data.frame(Name = c("Faro, Portugal, NE Atlantic" ,
                        Lat = c(37.047, 27.794,15.957,23.040 ) )
 my.sites
 
-# Visualize on Gol map
+# Visualize on Gl map
 
 m <- leaflet()
 m <- addTiles(m)
