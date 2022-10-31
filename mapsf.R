3e5a53a8fd42f80f9a5b672e8946506daf88cce5
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-10-31 21:13:01 +0300

    Working with mapsf

--- mapsf.R
+++ mapsf.R
@@ -1,7 +1,7 @@
 # Task: Creating maps and inset maps
 # Author: Wyclife Agumba Oluoch
 # Contact: https://github.com/Wycology
-# Last edited 2th October 2022 
+# Last edited th October 2022 
 
 library(mapsf) # Loads the main mapping package.
 
