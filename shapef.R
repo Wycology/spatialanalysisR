a781bab4af549cb8e0be527c8802af4844969ceb
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-11-03 20:04:11 +0300

    Shape R

--- shapef.R
+++ shapef.R
@@ -2,46 +2,22 @@
 # Created on 30th August 2021                      #
 # Last edited on 16th September 2022 by the author #
 
-library(rgdal)        # Version 1.5.32
+library(sf)           # Version 1.5.32
 library(tidyverse)    # Version 1.3.2
 library(broom)        # Version 1.0.1
 library(patchwork)    # Version 1.1.2
 
-my_shapefile <- readOGR('shp/dummy.shp')
+my_shapefile <- st_read('shp/dummy.shp')
 
 mode(my_shapefile)     # This is S4 object.
 length(my_shapefile)   # Number of features in the layer...like two roads
 
-my_tibble <- tidy(my_shapefile) # Tidy the shapefile 
+head(my_shapefile) 
 
-head(my_tibble) 
+nrow(my_shapefile)
 
-nrow(my_tibble)
+# Making map -------------------------------------------------------------------------------------
 
-# Making map
+my_shapefile |> ggplot() +
+  geom_sf()
 
-my_tibble |> ggplot(aes(long, lat, group = group, col = group)) +
-  geom_path()
-my_tibble |> ggplot(aes(long, lat, group = group, col = group)) +
-  coord_map('mercator') +
-  geom_path()
-
-# Plotting regardless of coordinates
-a <- my_tibble |> ggplot(aes(long, lat, group = group, col = group)) +
-  geom_path() +
-  labs(x = "Longitude", y = "Latitude")
-
-# Consideration the spatial nature of the data
-b <- my_tibble |> ggplot(aes(long, lat, group = group, col = group)) +
-  coord_map('mercator') +
-  geom_path() +
-  labs(x = "Logitude", y = "Latitude")
-
-b / a
-
-# All done
-# sinusoidal, cylequalarea, cylindrical, rectangular, gall, mollweide, gilbert,
-# azequidistant, gnomonic, perspective, orthographic, stereographic, laue, fisheye,
-# newyorker, conic, simpleconic, lambert, albers, bonne, polyconic, aitoff, lagrange,
-# bicentric, elliptic, globular, vandergrinten, eisenlohr, guyou, square, tetra,
-# harrison, trapezoidal, lune, mecca, homing, sp\_mercator, sp\_albers
