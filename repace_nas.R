dce2b72919a5b119b4e67a872dd13eae2ebdb5c1
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-11-13 21:24:32 +0300

    replacing NAs in R

--- repace_nas.R
+++ repace_nas.R
@@ -1,4 +1,4 @@
-# Replacing NA values with median values in every column o tibble object in R
+# Replacing NA values with median values in every column of tibble object in R
 
 library(dplyr)      # Version 1.0.10
 library(tidyr)      # Version 1.2.1
