dc1184cbfad708917aa5645b6205c4cd0888da5a
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-11-18 21:36:07 +0300

    Turkana dams script

--- Turkana_waterpoints.R
+++ Turkana_waterpoints.R
@@ -1,7 +1,7 @@
 ####################################################
 # Created by Wyclife Agumba Oluoch                 #     
 # Created on 22nd April 2021                       #
-# Last edited on 18 November 2022                 #
+# Last edited on 18t November 2022                 #
 # Task: Mapping open water points in Turkana County#
 ####################################################
 
