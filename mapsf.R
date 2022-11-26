7133c4c25934e718023761be041647d6dbf8c535
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-11-26 21:22:53 +0300

    Improving on code readability of spatial analysis with R

--- mapsf.R
+++ mapsf.R
@@ -3,7 +3,7 @@
 # Contact: https://github.com/Wycology
 # Last edited 26th November 2022 
 
-library(mapsf) # Loadi the main mapping package.
+library(mapsf) # Loadin the main mapping package.
 
 mf_theme(
   x = 'candy',
