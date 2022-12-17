8a22a1d835738e30bba7ab51cd9d94e483d84df2
Wycology <wyclifeoluoch@gmail.com>
2022-12-17 20:33:43 +0300

    Working with spatial lag in R

--- lag.R
+++ lag.R
@@ -1,6 +1,6 @@
 # Task: Calculating spatial lag in R
 # Author: Wyclife Agumba Oluoch
-# : https://github.com/Wycology
+# S: https://github.com/Wycology
 # Last edited: 17th December 2022
 
 library(sf) # Version 
