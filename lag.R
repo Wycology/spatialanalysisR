f59d833346593e814936e831f13a5d5507fbf229
Wycology <wyclifeoluoch@gmail.com>
2023-02-04 08:25:36 +0300

    Lag R

--- lag.R
+++ lag.R
@@ -44,8 +44,7 @@ gg_crime_obs <- ggplot(guerry, aes(fill = crime_pers)) +
 
 crime_lags <- guerry %>% 
   mutate(
-    nb = st_contiguity(geometry),
-    wt = st_weights(nb),
+    nb = st_contiguity(geometry), wt = st_weights(nb),
     crime_lag = st_lag(crime_pers, nb, wt)
   )
 
