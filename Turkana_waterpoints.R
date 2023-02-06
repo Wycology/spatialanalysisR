59104517091e5a09d637a67d8e56fc9a6cff98b1
Wycology <wyclifeoluoch@gmail.com>
2023-02-06 20:54:04 +0300

    update dams code

--- Turkana_waterpoints.R
+++ Turkana_waterpoints.R
@@ -1,7 +1,7 @@
 ####################################################
 # Created by Wyclife Agumba Oluoch                 #     
 # Created on 22nd April 2021                       #
-# Last edited on 5th F 2023                         #
+# Last edited on 5th Fe 2023                         #
 # Task: Mapping Watering points in Turkana County  #
 ####################################################
 
