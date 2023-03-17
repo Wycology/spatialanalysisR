3176609c2b4ec819ded69e00d24fba1cfa58aede
Wycology <wyclifeoluoch@gmail.com>
2023-03-17 23:56:02 +0300

    Turkana water

--- Turkana_waterpoints.R
+++ Turkana_waterpoints.R
@@ -1,7 +1,7 @@
 ####################################################
 # Created by Wyclife Agumba Oluoch                 #     
 # Created on 22nd April 2021                       #
-# Last edited on 17th F 2023                 #
+# Last edited on 17th  2023                 #
 # Task: Mapping Watering points in Turkana County  #
 ####################################################
 
