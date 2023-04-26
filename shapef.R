f7320432f0c4595c6026e82946bf436f373eafc4
Wycology <wyclifeoluoch@gmail.com>
2023-04-26 23:03:36 +0200

    shapefile in R

--- shapef.R
+++ shapef.R
@@ -9,7 +9,7 @@ library(ggplot2)      # Version 3.4.2
 
 my_shapefile <- st_read('shp/dummy.shp')
 
-mode(my_shapefile)     # This is .
+mode(my_shapefile)     # This i.
 length(my_shapefile)   # Number of features in the layer...like two roads
 
 head(my_shapefile) 
