c620b045bcbf368e19c48083c8edb60ef3ed8e56
Wycology <wyclifeoluoch@gmail.com>
2023-05-05 23:05:20 +0200

    Pseudo-absence sampling from donought buffers

--- doughnut.R
+++ doughnut.R
@@ -1,2 +1,20 @@
 # Extracting pseudo-absence from doughnut buffer around presence points
 
+library(sf)
+library(tidyverse)
+library(charlatan)
+
+presence <- data.frame(lon = ch_lon(n = 20), lat = ch_lat(n = 20)) |> 
+  st_as_sf(coords = c("lon", "lat"))
+
+plot(presence)
+
+large_buffer <- st_union(st_buffer(presence, 20))
+plot(large_buffer, add = T)
+
+small_buffer <- st_union(st_buffer(presence, 9))
+plot(small_buffer, add = T)
+
+doughnut <- st_difference(large_buffer, small_buffer)
+
+plot(doughnut, add = T)
