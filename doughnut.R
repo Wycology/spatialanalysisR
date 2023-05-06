c0ff9e3f9fa983dc2ccf8fb98dc8aa1bc2e867af
Wycology <wyclifeoluoch@gmail.com>
2023-05-06 23:54:23 +0200

    Donought plots in R

--- doughnut.R
+++ doughnut.R
@@ -1,7 +1,7 @@
 # Extracting pseudo-absence from doughnut buffer around presence points
 
 library(sf) # For wrangling simple features
-library(tidyverse) # 
+library(tidyverse) # Manipulating 
 library(charlatan)
 
 presence <- data.frame(lon = ch_lon(n = 20), lat = ch_lat(n = 20)) |> 
