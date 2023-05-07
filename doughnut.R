08f0dcb450e4a69f9b1ee9625083d4b635c5f90f
Wycology <wyclifeoluoch@gmail.com>
2023-05-07 23:16:51 +0200

    Manipulating doughnut plot and sampling background data.

--- doughnut.R
+++ doughnut.R
@@ -1,7 +1,7 @@
 # Extracting pseudo-absence from doughnut buffer around presence points
 
 library(sf) # For wrangling simple features object
-library(tidyverse) # Manipulating data frames or tibbl
+library(tidyverse) # Manipulating data frames or tib
 library(charlatan) # For generating random points
 
 presence <- data.frame(lon = ch_lon(n = 20), lat = ch_lat(n = 20)) |> 
