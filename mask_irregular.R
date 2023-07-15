e0cc41598c06c7d132cfa7021f49542625002ced
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2023-07-15 22:54:14 +0200

    Update mask_irregular.R

--- mask_irregular.R
+++ mask_irregular.R
@@ -18,7 +18,7 @@ library(sf)      # version 1.0.4
 
 worldclim <- getData('worldclim', var = 'tmin', res = 10)
 
-# study area boundary  from GADM database
+# Study area boundary  from GADM database
 
 kenya <- getData('GADM', country = 'KEN', level = 0)
 
