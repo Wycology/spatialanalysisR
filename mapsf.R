326bcc1e6bcd4e5cc91e9d5f0bd9f408d86e9b0f
Wycology <wyclifeoluoch@gmail.com>
2023-08-05 22:46:25 +0200

    Cleaning the code

--- mapsf.R
+++ mapsf.R
@@ -7,8 +7,7 @@
 pacman::p_load(mapsf) # Load the main map creation package
 
 mf_theme(x = 'candy', bg = 'cyan', fg = 'purple',
-  pos = 'center', inner = TRUE,
-  line = 2,
+  pos = 'center', inner = TRUE, line = 2,
   cex = 3,
   font = 16,
   mar = c(0, 0, 0, 0),
