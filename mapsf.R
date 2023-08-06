7e66e6c5164d091e1fc1afcfd16d1409bfde39b7
Wycology <wyclifeoluoch@gmail.com>
2023-08-06 22:28:44 +0200

    Formating text

--- mapsf.R
+++ mapsf.R
@@ -18,7 +18,7 @@ mtq$geom
 mtq_target <- mtq[26,] 
 
 mtq_target <-
-  mtq |> dplyr::filter(LIBGEO == 'Sainte-Anne') 
+mtq |> dplyr::filter(LIBGEO == 'Sainte-Anne') 
 
 mf_choro(mtq, var = 'POP', nbreaks = 5)
 
