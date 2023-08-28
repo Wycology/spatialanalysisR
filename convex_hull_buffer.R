ae7721a401978eecc20eafeaca9ce46635e31834
Wycology <wyclifeoluoch@gmail.com>
2023-08-28 23:26:56 +0200

    Creating convex hull and buffer around species occurrence points

--- convex_hull_buffer.R
+++ convex_hull_buffer.R
@@ -1,5 +1,5 @@
 # Library ----------------------------------------------------
-pacman::p_load(sf)
+pacman::p_load(sf) # 
 
 # Occurrence records -----------------------------------------
 species <-
