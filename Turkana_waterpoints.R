488a47ad43e84bdb16d5b88d4cffdae6004befb7
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2023-09-17 21:49:11 +0200

    Update Turkana_waterpoints.R

--- Turkana_waterpoints.R
+++ Turkana_waterpoints.R
@@ -1,7 +1,7 @@
 ####################################################
 # Created by Wyclife Agumba Oluoch                 #     
 # Created on 22nd April 2021                       #
-# Last edited on 17th S 2023                   #
+# Last edited on 17th Se 2023                   #
 # Task: Watering points in Turkana County  #
 ####################################################
 
