358af295b4bcab10622652033edf1869b4587efd
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2023-10-01 20:24:48 +0200

    Update convex_hull_buffer.R

--- convex_hull_buffer.R
+++ convex_hull_buffer.R
@@ -1,5 +1,5 @@
 # Library ----------------------------------------------------
-pacman::p_load(sf) # For spatial analysi, Version 1.0.14
+pacman::p_load(sf) # For spatial analysis, Version 1.0.14
 
 # Occurrence records -----------------------------------------
 species <-
