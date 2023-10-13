cf77ad818eaa9cd65f234f150aaa06d418fed205
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2023-10-13 22:54:47 +0200

    Update doughnut.R

--- doughnut.R
+++ doughnut.R
@@ -1,6 +1,6 @@
 # Extracting pseudo-absence from doughnut buffer around presence points
 
-library(sf) # For wrangling tsimple features object
+library(sf) # For wrangling thsimple features object
 library(tidyverse) # Wrangling data frames
 library(charlatan) # For generating random points
 
