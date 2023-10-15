4059e0e35f2522e8532b3ac5238a2fd54b6bded4
Oluoch, Wyclife Agumba <wyclifeoluoch@gmail.com>
2023-10-15 20:54:09 +0200

    Update workingwitspatialR.R

--- workingwitspatialR.R
+++ workingwitspatialR.R
@@ -6,7 +6,7 @@
 # +254729371248                        #
 # TASK: Working with spatial data in R #
 # Created on 28th Dec 2020 4.24 pm     #
-# Last modified on 15th O 2021       #
+# Last modified on 15th Oc 2021       #
 ########################################
 
 # Calculating plant canopy heights across NEON sites in Harvard using dsm & dtm.
