62afa90450efd39d89f2f3ea5acba3b960ef5b2c
Wycology <wyclifeoluoch@gmail.com>
2023-11-04 18:32:39 +0300

    Making a function to achieve the task of convex hull and plotting

--- convex_hull_buffer.R
+++ convex_hull_buffer.R
@@ -39,7 +39,6 @@ plot(st_geometry(species), col = "blue", pch = 19, add = TRUE)
 plot(st_geometry(bg_points), col = "#e31a1c", pch = 16, add = TRUE)
 plot(st_geometry(centroid), col = "yellow", pch = 16, cex = 4, add = TRUE)
 
-
 # Write a function --------------------------------------------------------
 
 for (i in 1:20) {
